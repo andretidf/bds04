1962a986ad678daf4c040b13529f12a8dea2ec0a
Andre Muniz <andretidf@gmail.com>
2022-09-09 18:02:54 -0300

    preparando security, h2, service basic user

---
+++ src/main/java/com/devsuperior/bds04/services/UserService.java
@@ -0,0 +1,51 @@
+package com.devsuperior.bds04.services;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds04.controllers.UserRepository;
+import com.devsuperior.bds04.dto.UserDTO;
+import com.devsuperior.bds04.entities.User;
+
+
+@Service
+public class UserService implements UserDetailsService{
+	
+	private static Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
+	
+	
+	@Autowired
+	private UserRepository repository;
+	
+	
+
+	@Transactional(readOnly = true)
+	public Page<UserDTO> findAllPaged(Pageable pageable){
+		
+		Page<User> list = repository.findAll(pageable);
+		
+		return list.map(x -> new UserDTO(x));
+		
+	}
+
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		
+		if(user == null) {
+			logger.error("User not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("User found: " + username);
+		return user;
+	}
+}
